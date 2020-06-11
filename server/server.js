720430b02d44ad6f6ffbae5783645ffcf380c025
jimchn <jimmychen12249@gmail.com>
2020-06-11 12:52:48 -0400

    setup boilerplate

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Create Route
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/index.html'));
+})
+
+
+
+//redirects any unknown routes to a 404 status
+app.use('*', (req, res) =>{
+    return res.status(404);
+});
+
+app.use((err, req, res, next) => {
+    const defaultErr = {
+        log: "An global error has occured",
+        status: 400,
+        err: { err: 'An error has occured in the server'}
+    };
+    Object.assign(defaultErr, err);
+    return res.status(defaultErr.status).json(defaultErr);
+});
+
+// Check if connected logic
+app.listen(PORT, () => {
+    console.log(`You are now connected to Port: ${PORT}`);
+})
+
+
+module.exports = app;
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+    entry: './client/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    mode: process.env.NODE_ENV,
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options:{
+                        presets:[
+                            '@babel/preset-env',
+                            '@babel/preset-react'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    devServer: {
+        publicPath: '/build/',
+        contentBase: path.join(__dirname, 'client'),
+        port: 8080,
+        proxy: {
+            '/' : "http://localhost:3000"
+        }
+    }
+}
